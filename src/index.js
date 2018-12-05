5fd6a1488d62e779c2df8d467a641ee2049e1bda
jeongjoonsup <penny4860@gmail.com>
2018-12-05 21:11:07 +0900

    2-1 : 다른 component 를 index.js 에서 불러오는 주석

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// index.js : 다른 component를 rendering 하는 역할
+//  1. 다른 component 를 불러오는 방법
+//      1) component의 class 를 import
+//      2) "<App />" 로 componet class 를 사용
+
+// ReactDOM 이 "App" 이라는 component 를 index.html에 <root>에 rendering
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

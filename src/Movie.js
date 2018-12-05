6a8b4281cf63e9c7780b0a02ced76c13eae88b8f
jeongjoonsup <penny4860@gmail.com>
2018-12-05 21:55:23 +0900

    2-4 : prop-types를 명시

--- src/Movie.js
+++ src/Movie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 // component 를 구현하는 방법
@@ -6,6 +7,13 @@ import './Movie.css';
 //  2. component class 구현
 //  3. render() 구현
 class Movie extends Component{
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+    }
+
+
     render(){
         return(
             <div>
@@ -17,6 +25,11 @@ class Movie extends Component{
 }
 
 class MoviePoster extends Component{
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render(){
         return(
             <img src={this.props.poster} alt="Movie Poster" />

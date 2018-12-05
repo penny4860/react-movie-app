f7d27be6423408ba43f6a536c42e0060af78edad
jeongjoonsup <penny4860@gmail.com>
2018-12-05 21:13:15 +0900

    2-1: component 를 구현하는 방법 comments

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './Movie.css';
 
+// component 를 구현하는 방법
+//  1. import react
+//  2. component class 구현
+//  3. render() 구현
 class Movie extends Component{
     render(){
         return(

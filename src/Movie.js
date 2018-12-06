ca9efb27d760d710abf2ebc0c74b7df55a593c70
jeongjoonsup <penny4860@gmail.com>
2018-12-06 20:54:08 +0900

    5. Stateless Functional Components 주석

--- src/Movie.js
+++ src/Movie.js
@@ -2,27 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-// component 를 구현하는 방법
-//  1. import react
-//  2. component class 구현
-//  3. render() 구현
-// class Movie extends Component{
-
-//     // propTypes : Movie component 가 props로 전달받는 정보의 type/필수여부를 정의
-//     static propTypes = {
-//         title: PropTypes.string.isRequired,
-//         poster: PropTypes.string.isRequired
-//     }
-
-
-//     render(){
-//         return(
-//             <div>
-//                 <MoviePoster poster={this.props.poster} />
-//                 <h1>{this.props.title}</h1>
-//             </div>
-//         )
-//     }
+// Component class 에 state가 없고, props 만 있을 경우,
+// function으로 바꾸고, props를 parameter로 전달받자.
 function Movie({title, poster}){
     return (
         <div>
@@ -36,15 +17,6 @@ function MoviePoster({poster}){
     return (
         <img src={poster} alt="Movie Poster" />
     )
-}
-
-Movie.propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-}
-
-MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
 }
 
 export default Movie
